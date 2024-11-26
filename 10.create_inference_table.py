736159666ccd5199276f3e4cfeccc6ccd703339b
deg <annikadenninger@yahoo.de>
2024-11-26 17:06:21 +0100

    first version of 11

--- 10.create_inference_table.py
+++ 10.create_inference_table.py
@@ -34,56 +34,15 @@ catalog_name = config.catalog_name
 schema_name = config.schema_name
 pipeline_id = config.pipeline_id
 
+print("Load data sets")
 # Ensure 'Id' column is cast to string in Spark before converting to Pandas
 train_set = spark.table(f"{catalog_name}.{schema_name}.train_set_an") \
-                 .withColumn("pickup_zip", col("pickup_zip").cast("string")) \
+                 .select(col("pickup_zip").cast("string"), col("trip_distance").cast("integer")) \
                  .toPandas()
 
-test_set = spark.table(f"{catalog_name}.{schema_name}.test_set") \
-                 .withColumn("pickup_zip", col("pickup_zip").cast("string")) \
+test_set = spark.table(f"{catalog_name}.{schema_name}.test_set_an") \
+                 .select(col("pickup_zip").cast("string"), col("trip_distance").cast("integer")) \
                  .toPandas()
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC ## Get the most important features using random forest model, NOT NECESSARY BECAUSE I ONLY HAVE ONE FEATURE!
-
-# COMMAND ----------
-
-""" import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import LabelEncoder
-
-# Encode categorical and datetime variables
-def preprocess_data(df):
-    label_encoders = {}
-    for col in df.select_dtypes(include=['object', 'datetime']).columns:
-        le = LabelEncoder()
-        df[col] = le.fit_transform(df[col].astype(str))
-        label_encoders[col] = le
-    return df, label_encoders
-
-train_set, label_encoders = preprocess_data(train_set)
-
-# Define features and target (adjust columns accordingly)
-features = train_set.drop(columns=["SalePrice"])
-target = train_set["SalePrice"]
-
-# Train a Random Forest model
-model = RandomForestRegressor(random_state=42)
-model.fit(features, target)
-
-# Identify the most important features
-feature_importances = pd.DataFrame({
-    'Feature': features.columns,
-    'Importance': model.feature_importances_
-}).sort_values(by='Importance', ascending=False)
-
-print("Top 5 important features:")
-print(feature_importances.head(5)) """
-
-
 
 # COMMAND ----------
 
@@ -91,6 +50,8 @@ print(feature_importances.head(5)) """
 # MAGIC #### Generate 2 synthetic datasets, similar distribution to the existing data and skewed
 
 # COMMAND ----------
+
+print("Create synthetic data")
 
 from pandas.api.types import is_numeric_dtype, is_datetime64_any_dtype, is_object_dtype
 import numpy as np
@@ -107,35 +68,9 @@ def create_synthetic_data(df, drift=False, num_rows=100):
             synthetic_data[column] = np.random.normal(mean, std, num_rows)
             synthetic_data[column] = synthetic_data[column].astype(dtype)  # Ensure correct data type
 
-        elif is_object_dtype(dtype) or str(dtype).startswith("category"):
-            # Generate categorical values based on the distribution in the source data
-            synthetic_data[column] = np.random.choice(df[column].unique(), num_rows, 
-                                                      p=df[column].value_counts(normalize=True))
-
-        elif is_datetime64_any_dtype(dtype):
-            # Generate random dates within the range of existing values
-            min_date, max_date = df[column].min(), df[column].max()
-            if min_date < max_date:
-                synthetic_data[column] = pd.to_datetime(
-                    np.random.randint(min_date.value, max_date.value, num_rows)
-                )
-            else:
-                synthetic_data[column] = [min_date] * num_rows
-
         else:
             # For other types, replicate random samples from existing data
             synthetic_data[column] = np.random.choice(df[column], num_rows)
-
-    # # Generate unique IDs
-    # new_ids = []
-    # i = max(existing_ids) + 1 if existing_ids else 1
-    # while len(new_ids) < num_rows:
-    #     if i not in existing_ids:
-    #         new_ids.append(i)
-    #     i += 1
-    # synthetic_data['Id'] = new_ids
-
-    # existing_ids.update(new_ids)  # Add this line to update the existing IDs set
 
     if drift:
         # Skew the top features to introduce drift
@@ -148,11 +83,8 @@ def create_synthetic_data(df, drift=False, num_rows=100):
 
 # Generate and visualize fake data
 
-combined_set = pd.concat([train_set, test_set], ignore_index=True)
-existing_ids = set(int(id) for id in combined_set['Id'])
-
-synthetic_data_normal = create_synthetic_data(train_set_an,  drift=False, num_rows=200)
-synthetic_data_skewed = create_synthetic_data(train_set_an, drift=True, num_rows=200)
+synthetic_data_normal = create_synthetic_data(train_set,  drift=False, num_rows=200)
+synthetic_data_skewed = create_synthetic_data(train_set, drift=True, num_rows=200)
 
 print(synthetic_data_normal.dtypes)
 print(synthetic_data_normal.head())
@@ -163,6 +95,7 @@ print(synthetic_data_normal.head())
 # MAGIC ### Add timestamp
 
 # COMMAND ----------
+print("Change update_timestamp_utc")
 
 synthetic_normal_df = spark.createDataFrame(synthetic_data_normal)
 synthetic_normal_df_with_ts = synthetic_normal_df.withColumn(
@@ -189,6 +122,7 @@ synthetic_skewed_df_with_ts.write.mode("append").saveAsTable(
 
 # COMMAND ----------
 
+print("Change features_an")
 import time
 from databricks.sdk import WorkspaceClient
 
@@ -201,25 +135,25 @@ spark.sql(f"""
     FROM {catalog_name}.{schema_name}.inference_set_normal_an
 """)
 
-#write into feature table; update online table
-spark.sql(f"""
-    INSERT INTO {catalog_name}.{schema_name}.features_an
-    SELECT pickup_zip, trip_distance
-    FROM {catalog_name}.{schema_name}.inference_set_skewed_an
-""")
+# #write into feature table; update online table
+# spark.sql(f"""
+#     INSERT INTO {catalog_name}.{schema_name}.features_an
+#     SELECT pickup_zip, trip_distance
+#     FROM {catalog_name}.{schema_name}.inference_set_skewed_an
+# """)
   
-update_response = workspace.pipelines.start_update(
-    pipeline_id=pipeline_id, full_refresh=False)
-while True:
-    update_info = workspace.pipelines.get_update(pipeline_id=pipeline_id, 
-                            update_id=update_response.update_id)
-    state = update_info.update.state.value
-    if state == 'COMPLETED':
-        break
-    elif state in ['FAILED', 'CANCELED']:
-        raise SystemError("Online table failed to update.")
-    elif state == 'WAITING_FOR_RESOURCES':
-        print("Pipeline is waiting for resources.")
-    else:
-        print(f"Pipeline is in {state} state.")
-    time.sleep(30)
+# update_response = workspace.pipelines.start_update(
+#     pipeline_id=pipeline_id, full_refresh=False)
+# while True:
+#     update_info = workspace.pipelines.get_update(pipeline_id=pipeline_id, 
+#                             update_id=update_response.update_id)
+#     state = update_info.update.state.value
+#     if state == 'COMPLETED':
+#         break
+#     elif state in ['FAILED', 'CANCELED']:
+#         raise SystemError("Online table failed to update.")
+#     elif state == 'WAITING_FOR_RESOURCES':
+#         print("Pipeline is waiting for resources.")
+#     else:
+#         print(f"Pipeline is in {state} state.")
+#     time.sleep(30)
---
+++ 11.send_request_to_endpoint.py
@@ -0,0 +1,129 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Send request to the endpoint from normal and skewed distribution
+
+# COMMAND ----------
+
+# MAGIC %pip install /Volumes/main/default/file_exchange/denninger/nyc_taxi-0.0.1-py3-none-any.whl
+
+# COMMAND ----------
+
+# MAGIC %restart_python
+
+# COMMAND ----------
+
+import pandas as pd
+from pyspark.sql.functions import col
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
+import numpy as np
+import datetime
+import itertools
+from pyspark.sql import SparkSession
+
+from nyctaxi.config import ProjectConfig
+
+spark = SparkSession.builder.getOrCreate()
+
+# Load configuration
+config = ProjectConfig.from_yaml(config_path="project_config.yml")
+catalog_name = config.catalog_name
+schema_name = config.schema_name
+
+
+inference_data_normal = spark.table(f"{catalog_name}.{schema_name}.inference_set_normal_an") \
+                        .withColumn("pickup_zip", col("pickup_zip").cast("string")) \
+                        .toPandas()
+inference_data_skewed = spark.table(f"{catalog_name}.{schema_name}.inference_set_skewed_an") \
+                        .withColumn("pickup_zip", col("pickup_zip").cast("string")) \
+                        .toPandas()
+
+test_set = spark.table(f"{catalog_name}.{schema_name}.test_set_an") \
+                        .withColumn("pickup_zip", col("pickup_zip").cast("string")) \
+                        .toPandas()
+
+
+# COMMAND ----------
+print("Does token work?")
+
+token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+host = spark.conf.get("spark.databricks.workspaceUrl")
+
+
+# COMMAND ----------
+
+from databricks.sdk import WorkspaceClient
+import requests
+import time
+
+workspace = WorkspaceClient()
+
+# Required columns for inference
+required_columns = [
+    "trip_distance",
+    "pickup_zip",
+]
+
+# Sample records from inference datasets
+sampled_normal_records = inference_data_normal[required_columns].to_dict(orient="records")
+sampled_skewed_records = inference_data_skewed[required_columns].to_dict(orient="records")
+test_set_records = test_set[required_columns].to_dict(orient="records")
+
+# Two different way to send request to the endpoint
+# 1. Using https endpoint
+def send_request_https(dataframe_record):
+    model_serving_endpoint = f"https://{host}/serving-endpoints/nyctaxi-model-serving-fe/invocations"
+    response = requests.post(
+        model_serving_endpoint,
+        headers={"Authorization": f"Bearer {token}"},
+        json={"dataframe_records": [dataframe_record]},
+    )
+    return response
+
+# # 2. Using workspace client
+# def send_request_workspace(dataframe_record):
+#     response = workspace.serving_endpoints.query(
+#         name="nyctaxi-model-serving-fe",
+#         dataframe_records=[dataframe_record]
+#     )
+#     return response
+
+
+# COMMAND ----------
+
+# Loop over test records and send requests for 20 minutes
+end_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
+for index, record in enumerate(itertools.cycle(test_set_records)):
+    if datetime.datetime.now() >= end_time:
+        break
+    print(f"Sending request for test data, index {index}")
+    response = send_request_https(record)
+    print(f"Response status: {response.status_code}")
+    print(f"Response text: {response.text}")
+    time.sleep(0.2)
+
+# COMMAND ----------
+
+# Loop over normal records and send requests for 10 minutes
+end_time = datetime.datetime.now() + datetime.timedelta(minutes=20)
+for index, record in enumerate(itertools.cycle(sampled_normal_records)):
+    if datetime.datetime.now() >= end_time:
+        break
+    print(f"Sending request for normal data, index {index}")
+    response = send_request_https(record)
+    print(f"Response status: {response.status_code}")
+    print(f"Response text: {response.text}")
+    time.sleep(0.2)
+
+
+# COMMAND ----------
+
+# Loop over skewed records and send requests for 30 minutes
+end_time = datetime.datetime.now() + datetime.timedelta(minutes=30)
+for index, record in enumerate(itertools.cycle(sampled_skewed_records)):
+    if datetime.datetime.now() >= end_time:
+        break
+    print(f"Sending request for skewed data, index {index}")
+    response = send_request_https(record)
+    print(f"Response status: {response.status_code}")
+    print(f"Response text: {response.text}")
+    time.sleep(0.2)
